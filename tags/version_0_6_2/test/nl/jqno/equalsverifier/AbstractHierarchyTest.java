f71a8762635a36c4225b7e3cb3b5247bf9e2d021
jan.ouwens <jan.ouwens@5f335f74-4ea7-11de-a6a3-df26d2c71dbe>
2010-02-11 19:59:25 +0000

    Tagged: version 0.6.2

---
+++ tags/version_0_6_2/test/nl/jqno/equalsverifier/AbstractHierarchyTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2009 Jan Ouwens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.jqno.equalsverifier;
+
+import nl.jqno.equalsverifier.points.Color;
+
+import org.junit.Test;
+
+public class AbstractHierarchyTest extends EqualsVerifierTestBase {
+	@Test
+	public void abstractFinalMethods() {
+		EqualsVerifier.forClass(AbstractFinalMethodsPoint.class).verify();
+	}
+	
+	@Test
+	public void abstractRedefinable() {
+		EqualsVerifier.forClass(AbstractRedefinablePoint.class)
+				.withRedefinedSubclass(FinalRedefinedPoint.class)
+				.verify();
+	}
+	
+	@Test
+	public void abstractNeverNull() {
+		EqualsVerifier.forClass(NullThrowingColorContainer.class)
+				.suppress(Warning.NULL_FIELDS)
+				.verify();
+		
+		EqualsVerifier<NullThrowingColorContainer> ev = EqualsVerifier.forClass(NullThrowingColorContainer.class);
+		verifyFailure("Non-nullity: equals throws NullPointerException", ev);
+	}
+	
+	static abstract class AbstractFinalMethodsPoint {
+		private final int x;
+		private final int y;
+		
+		AbstractFinalMethodsPoint(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public final boolean equals(Object obj) {
+			if (!(obj instanceof AbstractFinalMethodsPoint)) {
+				return false;
+			}
+			AbstractFinalMethodsPoint p = (AbstractFinalMethodsPoint)obj;
+			return x == p.x && y == p.y;
+		}
+		
+		@Override
+		public final int hashCode() {
+			return x + (31 * y);
+		}
+	}
+	
+	static abstract class AbstractRedefinablePoint {
+		private final int x;
+		private final int y;
+		
+		AbstractRedefinablePoint(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public boolean canEqual(Object obj) {
+			return obj instanceof AbstractRedefinablePoint;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof AbstractRedefinablePoint)) {
+				return false;
+			}
+			AbstractRedefinablePoint p = (AbstractRedefinablePoint)obj;
+			return p.canEqual(this) && x == p.x && y == p.y;
+		}
+		
+		@Override
+		public int hashCode() {
+			return x + (31 * y);
+		}
+		
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + ":" + x + "," + y;
+		}
+	}
+	
+	static final class FinalRedefinedPoint extends AbstractRedefinablePoint {
+		private final Color color;
+		
+		FinalRedefinedPoint(int x, int y, Color color) {
+			super(x, y);
+			this.color = color;
+		}
+		
+		@Override
+		public boolean canEqual(Object obj) {
+			return obj instanceof FinalRedefinedPoint;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof FinalRedefinedPoint)) {
+				return false;
+			}
+			FinalRedefinedPoint p = (FinalRedefinedPoint)obj;
+			return p.canEqual(this) && super.equals(p) && color == p.color;
+		}
+		
+		@Override
+		public int hashCode() {
+			return (color == null ? 0 : color.hashCode()) + (31 * super.hashCode());
+		}
+		
+		@Override
+		public String toString() {
+			return super.toString() + "," + color;
+		}
+	}
+	
+	static abstract class NullThrowingColorContainer {
+		private final Color color;
+		
+		public NullThrowingColorContainer(Color color) {
+			this.color = color;
+		}
+		
+		@Override
+		public final boolean equals(Object obj) {
+			if (!(obj instanceof NullThrowingColorContainer)) {
+				return false;
+			}
+			return color.equals(((NullThrowingColorContainer)obj).color);
+		}
+		
+		@Override
+		public final int hashCode() {
+			return color.hashCode();
+		}
+		
+		@Override
+		public String toString() {
+			return getClass() + ":" + color;
+		}
+	}
+}
---
+++ tags/version_0_6_2/test/nl/jqno/equalsverifier/util/InstantiatorInstantiationTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2009 Jan Ouwens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.jqno.equalsverifier.util;
+
+import static org.junit.Assert.assertFalse;
+import nl.jqno.equalsverifier.points.FinalPoint;
+import nl.jqno.equalsverifier.points.Point;
+import nl.jqno.equalsverifier.util.Instantiator;
+
+import org.junit.Test;
+
+public class InstantiatorInstantiationTest {
+	@Test
+	public void instantiateClass() {
+		Instantiator<Point> instantiator = Instantiator.forClass(Point.class);
+		instantiator.instantiate();
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void instantiateInterface() {
+		Instantiator.forClass(Interface.class);
+	}
+	
+	@Test
+	public void instantiateFinalClass() {
+		Instantiator.forClass(FinalPoint.class);
+	}
+	
+	@Test
+	public void instantiateArrayContainer() {
+		Instantiator.forClass(ArrayContainer.class);
+	}
+	
+	@Test
+	public void instantiateAbstractClass() {
+		Instantiator<AbstractClass> instantiator = Instantiator.forClass(AbstractClass.class);
+		instantiator.instantiate();
+	}
+	
+	@Test
+	public void instantiateSubclass() {
+		Instantiator<Point> instantiator = Instantiator.forClass(Point.class);
+		Point p = instantiator.instantiateSubclass();
+		assertFalse(p.getClass() == Point.class);
+	}
+	
+	static abstract class AbstractClass {
+		public int value = 42; 
+	}
+	
+	static interface Interface {}
+	
+	static class ArrayContainer {
+		public int[] array;
+	}
+}
