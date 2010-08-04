1436f34ffd3a7869056558813b930ffda6c1bf55
jan.ouwens <jan.ouwens@5f335f74-4ea7-11de-a6a3-df26d2c71dbe>
2010-08-04 21:11:38 +0000

    Issue 30: Recursive data structures in Java API classes -- added MORE

--- trunk/src/nl/jqno/equalsverifier/util/PrefabValues.java
+++ trunk/src/nl/jqno/equalsverifier/util/PrefabValues.java
@@ -15,19 +15,38 @@
  */
 package nl.jqno.equalsverifier.util;
 
+import java.text.DateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Formatter;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.TimeZone;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.WeakHashMap;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.regex.Pattern;
 
 import net.sf.cglib.proxy.NoOp;
@@ -42,10 +61,19 @@ class PrefabValues {
 	/**
 	 * @return A map of prefabricated object instances.
 	 */
-	@SuppressWarnings("unchecked")
 	public static Map<Class<?>, List<?>> get() {
 		Builder b = new Builder();
-		
+		addPrimitiveClasses(b);
+		addClasses(b);
+		addLists(b);
+		addMaps(b);
+		addSets(b);
+		addQueues(b);
+		addClassesNecessaryForCgLib(b);
+		return b.get();
+	}
+
+	private static void addPrimitiveClasses(Builder b) {
 		b.add(Boolean.class, true, false);
 		b.add(Byte.class, (byte)1, (byte)2);
 		b.add(Character.class, 'a', 'b');
@@ -54,14 +82,31 @@ class PrefabValues {
 		b.add(Integer.class, 1, 2);
 		b.add(Long.class, 1L, 2L);
 		b.add(Short.class, (short)1, (short)2);
-		
 		b.add(Object.class, new Object(), new Object());
 		b.add(Class.class, Class.class, Object.class);
 		b.add(String.class, "one", "two");
-		
+	}
+
+	private static void addClasses(Builder b) {
+		b.add(Calendar.class, new GregorianCalendar(2010, 7, 4), new GregorianCalendar(2010, 7, 5));
 		b.add(Date.class, new Date(0), new Date(1));
+		b.add(DateFormat.class, DateFormat.getTimeInstance(), DateFormat.getDateInstance());
+		b.add(Formatter.class, new Formatter(), new Formatter());
+		b.add(GregorianCalendar.class, new GregorianCalendar(2010, 7, 4), new GregorianCalendar(2010, 7, 5));
+		b.add(Locale.class, new Locale("nl"), new Locale("hu"));
 		b.add(Pattern.class, Pattern.compile("one"), Pattern.compile("two"));
-		
+		b.add(Scanner.class, new Scanner("one"), new Scanner("two"));
+		b.add(TimeZone.class, TimeZone.getTimeZone("GMT+1"), TimeZone.getTimeZone("GMT+2"));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void addLists(Builder b) {
+		b.add(CopyOnWriteArrayList.class, collection(new CopyOnWriteArrayList(), new CopyOnWriteArrayList()));
+		b.add(LinkedList.class, collection(new LinkedList(), new LinkedList()));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void addMaps(Builder b) {
 		b.add(EnumMap.class, Dummy.FIRST.map(), Dummy.SECOND.map());
 		b.add(ConcurrentHashMap.class, map(new ConcurrentHashMap(), new ConcurrentHashMap()));
 		b.add(HashMap.class, map(new HashMap(), new HashMap()));
@@ -70,9 +115,28 @@ class PrefabValues {
 		b.add(Properties.class, map(new Properties(), new Properties()));
 		b.add(TreeMap.class, map(new TreeMap(), new TreeMap()));
 		b.add(WeakHashMap.class, map(new WeakHashMap(), new WeakHashMap()));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void addSets(Builder b) {
+		b.add(CopyOnWriteArraySet.class, collection(new CopyOnWriteArraySet(), new CopyOnWriteArraySet()));
+		b.add(EnumSet.class, EnumSet.of(Dummy.FIRST), EnumSet.of(Dummy.SECOND));
+		b.add(TreeSet.class, collection(new TreeSet(), new TreeSet()));
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void addQueues(Builder b) {
+		b.add(ArrayBlockingQueue.class, new ArrayBlockingQueue(1), new ArrayBlockingQueue(1));
+		b.add(ConcurrentLinkedQueue.class, new ConcurrentLinkedQueue(), new ConcurrentLinkedQueue());
+		b.add(DelayQueue.class, new DelayQueue(), new DelayQueue());
+		b.add(LinkedBlockingQueue.class, new LinkedBlockingQueue(), new LinkedBlockingQueue());
+		b.add(PriorityBlockingQueue.class, new PriorityBlockingQueue(), new PriorityBlockingQueue());
+		b.add(SynchronousQueue.class, new SynchronousQueue(), new SynchronousQueue());
 		
-		b.add(NoOp.class, new NoOp(){}, new NoOp(){}); // Necessary for CGLib.
-		return b.get();
+	}
+
+	private static void addClassesNecessaryForCgLib(Builder b) {
+		b.add(NoOp.class, new NoOp(){}, new NoOp(){});
 	}
 	
 	private PrefabValues() {
@@ -83,6 +147,13 @@ class PrefabValues {
 	private static <T extends Map> List<T> map(T first, T second) {
 		first.put("first_key", "first_value");
 		second.put("second_key", "second_value");
+		return Arrays.asList(first, second);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static <T extends Collection> List<T> collection(T first, T second) {
+		first.add("first");
+		second.add("second");
 		return Arrays.asList(first, second);
 	}
 	
