a62df4d2d112f7a6837604b5a05fae9d9e673fa7
jan.ouwens <jan.ouwens@5f335f74-4ea7-11de-a6a3-df26d2c71dbe>
2010-10-31 16:52:33 +0000

    Added support for getClass() style equals methods: inheritance

--- trunk/test/nl/jqno/equalsverifier/GetClassTest.java
+++ trunk/test/nl/jqno/equalsverifier/GetClassTest.java
@@ -16,6 +16,10 @@
 package nl.jqno.equalsverifier;
 
 import static nl.jqno.equalsverifier.Helper.assertFailure;
+import static nl.jqno.equalsverifier.Helper.nullSafeHashCode;
+import nl.jqno.equalsverifier.points.Color;
+import nl.jqno.equalsverifier.points.FinalMethodsPoint;
+import nl.jqno.equalsverifier.points.Point;
 
 import org.junit.Test;
 
@@ -32,6 +36,31 @@ public class GetClassTest {
 		EqualsVerifier<GetClassPointNull> ev =
 				EqualsVerifier.forClass(GetClassPointNull.class).usingGetClass();
 		assertFailure(ev, "Non-nullity: NullPointerException thrown");
+	}
+	
+	@Test
+	public void useInstanceofWhenGetClassAnnounced() {
+		EqualsVerifier.forClass(FinalMethodsPoint.class)
+				.verify();
+		
+		EqualsVerifier<FinalMethodsPoint> ev =
+			EqualsVerifier.forClass(FinalMethodsPoint.class).usingGetClass();
+		assertFailure(ev, "Subclass", "object is equal to an instance of a trivial subclass with equal fields", "This should not happen when using getClass().");
+	}
+	
+	@Test
+	public void redefinedSubclassHappyPath() {
+		EqualsVerifier.forClass(GetClassColorPoint.class)
+				.usingGetClass()
+				.verify();
+	}
+	
+	@Test
+	public void equalSuperclass() {
+		EqualsVerifier<GetClassColorPointWithEqualSuper> ev =
+				EqualsVerifier.forClass(GetClassColorPointWithEqualSuper.class).usingGetClass();
+		assertFailure(ev, "Redefined superclass", GetClassColorPointWithEqualSuper.class.getSimpleName(),
+				"may not equal superclass instance", Point.class.getSimpleName(), "but it does");
 	}
 	
 	static class GetClassPointHappyPath {
@@ -79,6 +108,52 @@ public class GetClassTest {
 		@Override
 		public int hashCode() {
 			return x + (31 * y);
+		}
+	}
+	
+	static class GetClassColorPoint extends GetClassPointHappyPath {
+		private final Color color;
+		
+		public GetClassColorPoint(int x, int y, Color color) {
+			super(x, y);
+			this.color = color;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (super.equals(obj)) {
+				GetClassColorPoint other = (GetClassColorPoint)obj;
+				return color == other.color;
+			}
+			return false;
+		}
+		
+		@Override
+		public int hashCode() {
+			return super.hashCode() + nullSafeHashCode(color);
+		}
+	}
+	
+	static class GetClassColorPointWithEqualSuper extends Point {
+		private final Color color;
+		
+		public GetClassColorPointWithEqualSuper(int x, int y, Color color) {
+			super(x, y);
+			this.color = color;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null || obj.getClass() != getClass()) {
+				return false;
+			}
+			GetClassColorPointWithEqualSuper p = (GetClassColorPointWithEqualSuper)obj;
+			return super.equals(obj) && color == p.color;
+		}
+		
+		@Override
+		public int hashCode() {
+			return super.hashCode() + (31 * nullSafeHashCode(color));
 		}
 	}
 }
