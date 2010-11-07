bb40a5d88376e7d04ac68578b7dcca014096b9a9
jan.ouwens <jan.ouwens@5f335f74-4ea7-11de-a6a3-df26d2c71dbe>
2010-11-07 14:37:08 +0000

    Removed html from javadoc

--- trunk/src/nl/jqno/equalsverifier/Warning.java
+++ trunk/src/nl/jqno/equalsverifier/Warning.java
@@ -25,12 +25,12 @@ public enum Warning {
 	/**
 	 * Disables checks for non-final fields on which {@code equals} and
 	 * {@code hashCode} depend.
-	 * <p>
+	 * 
 	 * {@link EqualsVerifier}'s standard behaviour is to disallow non-final
 	 * fields being used in {@code equals} and {@code hashCode} methods, since
 	 * classes that depend on non-final fields in these methods cannot reliably
 	 * be used in collections.
-	 * <p>
+	 * 
 	 * However, sometimes an external library requires that fields be
 	 * non-final. An example of this are Java Beans. In such a case, this
 	 * method can be used to prevent {@link EqualsVerifier} from checking for
@@ -41,7 +41,7 @@ public enum Warning {
 	/**
 	 * Disables checks for {@link NullPointerException} within {@code equals},
 	 * {@code hashCode} and {@code toString} methods.
-	 * <p>
+	 * 
 	 * Sometimes the constructor of a class makes sure no field can be null.
 	 * If this is the case, and if the fields cannot be made null later in the
 	 * lifecycle of the class by setters or other methods, the
@@ -52,13 +52,13 @@ public enum Warning {
 	
 	/**
 	 * Disables some of the stricter inheritance tests; use at your own risk!
-	 * <p>
+	 * 
 	 * {@link EqualsVerifier}'s standard behaviour, if T is not final and
 	 * neither are its {@code equals} and {@code hashCode} methods, is to
 	 * require a reference to a subclass of T for which no instance can be
 	 * equal to any instance of T, to make sure that subclasses that can
 	 * redefine {@code equals} or {@code hashCode} don't break the contract.
-	 * <p>
+	 * 
 	 * Some may find that too strict for their liking; this method disables
 	 * that test.
 	 * 
