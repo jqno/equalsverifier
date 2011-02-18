456f9b838a6d720f33d1c800454e025d25bfe57a
Jan Ouwens <jan.ouwens@gmail.com>
2011-02-18 21:26:00 +0000

    Extracted interface for supported annotations enum

--- src/nl/jqno/equalsverifier/util/AnnotationAccessor.java
+++ src/nl/jqno/equalsverifier/util/AnnotationAccessor.java
@@ -28,6 +28,7 @@ import net.sf.cglib.asm.ClassReader;
 import net.sf.cglib.asm.ClassWriter;
 import net.sf.cglib.asm.FieldVisitor;
 import net.sf.cglib.asm.Type;
+import nl.jqno.equalsverifier.Annotation;
 
 /**
  * Provides access to the annotations that are defined on a class
@@ -54,14 +55,12 @@ class AnnotationAccessor {
 	/**
 	 * Determines whether {@link #type} has a particular annotation. 
 	 * 
-	 * @param annotationDescriptor The name of the annotation we want to find.
-	 * 			It can be in the form of a class name, or a (partial) fully
-	 * 			qualified class name.
+	 * @param annotation The annotation we want to find.
 	 * @return True if {@link #type} has an annotation with the supplied name.
 	 */
-	public boolean typeHas(String annotationDescriptor) {
+	public boolean typeHas(Annotation annotation) {
 		process();
-		return find(classAnnotations, annotationDescriptor);
+		return find(classAnnotations, annotation);
 	}
 	
 	/**
@@ -70,28 +69,28 @@ class AnnotationAccessor {
 	 *  
 	 * @param fieldName The name of the field for which we want to know if it
 	 * 			has the annotation.
-	 * @param annotationDescriptor The name of the annotation we want to find.
-	 * 			It can be in the form of a class name, or a (partial) fully
-	 * 			qualified class name.
+	 * @param annotation The annotation we want to find.
 	 * @return True if the specified field in {@link #type} has the specified
 	 * 			annotation.
 	 * @throws InternalException if {@link #type} does not have the specified
 	 * 			field.
 	 */
-	public boolean fieldHas(String fieldName, String annotationDescriptor) {
+	public boolean fieldHas(String fieldName, Annotation annotation) {
 		process();
 		Set<String> annotations = fieldAnnotations.get(fieldName);
 		if (annotations == null) {
 			throw new InternalException("Class " + type.getName() + " does not have field " + fieldName);
 		}
-		return find(annotations, annotationDescriptor);
+		return find(annotations, annotation);
 	}
 	
-	private boolean find(Set<String> annotations, String annotationDescriptor) {
-		String needle = annotationDescriptor.replaceAll("\\.", "/");
-		for (String haystack : annotations) {
-			if (haystack.contains(needle)) {
-				return true;
+	private boolean find(Set<String> annotations, Annotation annotation) {
+		for (String annotationDescriptor : annotation.descriptors()) {
+			String needle = annotationDescriptor.replaceAll("\\.", "/");
+			for (String haystack : annotations) {
+				if (haystack.contains(needle)) {
+					return true;
+				}
 			}
 		}
 		return false;
