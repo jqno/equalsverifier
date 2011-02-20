6d12c8b39c4c5b7544dbea90995860ece1a3400e
Jan Ouwens <jan.ouwens@gmail.com>
2011-02-20 14:24:34 +0000

    Q: Do annotations also apply to subclasses of the class they're defined on? A: Sometimes.

--- test/nl/jqno/equalsverifier/testhelpers/annotations/TestSupportedAnnotations.java
+++ test/nl/jqno/equalsverifier/testhelpers/annotations/TestSupportedAnnotations.java
@@ -17,25 +17,40 @@ package nl.jqno.equalsverifier.testhelpers.annotations;
 import java.util.Arrays;
 import java.util.List;
 
-import nl.jqno.equalsverifier.Annotation;
+import nl.jqno.equalsverifier.util.Annotation;
 
 public enum TestSupportedAnnotations implements Annotation {
-	TYPE_RUNTIME_RETENTION("Lnl/jqno/equalsverifier/testhelpers/annotations/TypeAnnotationRuntimeRetention;"),
-	TYPE_CLASS_RETENTION("Lnl/jqno/equalsverifier/testhelpers/annotations/TypeAnnotationClassRetention;"),
-	FIELD_RUNTIME_RETENTION("Lnl/jqno/equalsverifier/testhelpers/annotations/FieldAnnotationRuntimeRetention;"),
-	FIELD_CLASS_RETENTION("Lnl/jqno/equalsverifier/testhelpers/annotations/FieldAnnotationClassRetention;"),
-	TYPE_RUNTIME_RETENTION_PARTIAL_DESCRIPTOR("TypeAnnotationRuntimeRetention"),
-	TYPE_RUNTIME_RETENTION_CANONICAL_DESCRIPTOR(TypeAnnotationRuntimeRetention.class.getCanonicalName()),
-	FIELD_RUNTIME_RETENTION_PARTIAL_DESCRIPTOR("FieldAnnotationRuntimeRetention"),
-	FIELD_RUNTIME_RETENTION_CANONICAL_DESCRIPTOR(FieldAnnotationRuntimeRetention.class.getCanonicalName());
+	TYPE_RUNTIME_RETENTION(false, "Lnl/jqno/equalsverifier/testhelpers/annotations/TypeAnnotationRuntimeRetention;"),
+	TYPE_CLASS_RETENTION(false, "Lnl/jqno/equalsverifier/testhelpers/annotations/TypeAnnotationClassRetention;"),
+	FIELD_RUNTIME_RETENTION(false, "Lnl/jqno/equalsverifier/testhelpers/annotations/FieldAnnotationRuntimeRetention;"),
+	FIELD_CLASS_RETENTION(false, "Lnl/jqno/equalsverifier/testhelpers/annotations/FieldAnnotationClassRetention;"),
 	
+	TYPE_RUNTIME_RETENTION_PARTIAL_DESCRIPTOR(false, "TypeAnnotationRuntimeRetention"),
+	TYPE_RUNTIME_RETENTION_CANONICAL_DESCRIPTOR(false, TypeAnnotationRuntimeRetention.class.getCanonicalName()),
+	
+	FIELD_RUNTIME_RETENTION_PARTIAL_DESCRIPTOR(false, "FieldAnnotationRuntimeRetention"),
+	FIELD_RUNTIME_RETENTION_CANONICAL_DESCRIPTOR(false, FieldAnnotationRuntimeRetention.class.getCanonicalName()),
+	
+	TYPE_INHERITS(true, "TypeAnnotationInherits"),
+	TYPE_DOESNT_INHERIT(false, "TypeAnnotationDoesntInherit"),
+	FIELD_INHERITS(true, "FieldAnnotationInherits"),
+	FIELD_DOESNT_INHERIT(false, "FieldAnnotationDoesntInherit"),
+	;
+	
+	private final boolean inherits;
 	private final List<String> descriptors;
 
-	private TestSupportedAnnotations(String... descriptors) {
+	private TestSupportedAnnotations(boolean inherits, String... descriptors) {
+		this.inherits = inherits;
 		this.descriptors = Arrays.asList(descriptors);
 	}
 	
 	public Iterable<String> descriptors() {
 		return descriptors;
+	}
+	
+	@Override
+	public boolean inherits() {
+		return inherits;
 	}
 }
