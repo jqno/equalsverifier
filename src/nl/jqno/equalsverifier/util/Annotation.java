10288d2e923b4a7de70d8762b480d8d111e8810b
Jan Ouwens <jan.ouwens@gmail.com>
2011-02-20 17:28:29 +0000

    Added Javadoc

--- src/nl/jqno/equalsverifier/util/Annotation.java
+++ src/nl/jqno/equalsverifier/util/Annotation.java
@@ -15,7 +15,38 @@
  */
 package nl.jqno.equalsverifier.util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Describes an annotation that can be recognised by EqualsVerifier.
+ * 
+ * The annotation can have {@link RetentionPolicy#RUNTIME} or
+ * {@link RetentionPolicy#CLASS}, and must have either {@link ElementType#TYPE}
+ * or {@link ElementType#FIELD}.
+ * 
+ * @author Jan Ouwens
+ */
 public interface Annotation {
+	/**
+	 * One or more strings that contain the annotation's class name. A
+	 * descriptor can be the annotation's fully qualified canonical name, or a
+	 * substring thereof.
+	 * 
+	 * An annotation can be described by more than one descriptor. For
+	 * instance, @Nonnull, @NonNull and @NotNull have the same semantics; their
+	 * descriptors can be grouped together in one {@link Annotation} instance.
+	 * 
+	 * @return An Iterable of annotation descriptor strings.
+	 */
 	public Iterable<String> descriptors();
+	
+	/**
+	 * Whether the annotation applies to the class in which is appears only, or
+	 * whether it applies to that class and all its subclasses.
+	 *
+	 * @return True if the annotation is inherited by subclasses of the class
+	 * 			in which the annotation appears.
+	 */
 	public boolean inherits();
 }
