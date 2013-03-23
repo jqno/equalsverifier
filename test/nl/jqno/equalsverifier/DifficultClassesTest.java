75ad302ece374ab1eccf46fa2d8f0413aef9b0c9
Jan Ouwens <jan.ouwens@gmail.com>
2013-03-23 16:25:10 +0100

    Issue 74: Update copyright notices

--- test/nl/jqno/equalsverifier/DifficultClassesTest.java
+++ test/nl/jqno/equalsverifier/DifficultClassesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2012 Jan Ouwens
+ * Copyright 2009-2013 Jan Ouwens
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
