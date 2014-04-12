4db588a261834b3db9ad3e867661cdabab70df1c
Jan Ouwens <jan.ouwens@gmail.com>
2014-04-12 17:49:19 +0200

    Test cleanup: hashCode

--- test/nl/jqno/equalsverifier/integration/basiccontract/HashCodeTest.java
+++ test/nl/jqno/equalsverifier/integration/basiccontract/HashCodeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 Jan Ouwens
+ * Copyright 2009-2010, 2014 Jan Ouwens
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,22 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.jqno.equalsverifier;
+package nl.jqno.equalsverifier.integration.basiccontract;
 
 import static nl.jqno.equalsverifier.testhelpers.Util.assertFailure;
+import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.testhelpers.points.Point;
 
 import org.junit.Test;
 
 public class HashCodeTest {
 	@Test
-	public void invalidHashCode() {
-		EqualsVerifier<HashCodeBrokenPoint> ev = EqualsVerifier.forClass(HashCodeBrokenPoint.class);
-		assertFailure(ev, "hashCode: hashCodes should be equal", HashCodeBrokenPoint.class.getSimpleName());
+	public void fail_whenHashCodesAreUnequal_givenEqualObjects() {
+		EqualsVerifier<RandomHashCode> ev = EqualsVerifier.forClass(RandomHashCode.class);
+		assertFailure(ev, "hashCode: hashCodes should be equal", RandomHashCode.class.getSimpleName());
 	}
 	
-	static class HashCodeBrokenPoint extends Point {
-		public HashCodeBrokenPoint(int x, int y) {
+	static class RandomHashCode extends Point {
+		public RandomHashCode(int x, int y) {
 			super(x, y);
 		}
 
