3f8e89243f9b031183c20f8449987d2afb069295
Jan Ouwens <jan.ouwens@gmail.com>
2014-07-03 18:06:45 +0200

    Adds ConditionalInstantiator to instantiate classes that may or may not exist

---
+++ test/nl/jqno/equalsverifier/util/ConditionalInstantiatorTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2014 Jan Ouwens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.jqno.equalsverifier.util;
+
+import static nl.jqno.equalsverifier.util.ConditionalInstantiator.classes;
+import static nl.jqno.equalsverifier.util.ConditionalInstantiator.objects;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigDecimal;
+import java.util.GregorianCalendar;
+
+import nl.jqno.equalsverifier.util.ConditionalInstantiator;
+import nl.jqno.equalsverifier.util.exceptions.ReflectionException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ConditionalInstantiatorTest {
+	private ConditionalInstantiator ci;
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void resolveReturnsClass_whenTypeExists() {
+		ci = new ConditionalInstantiator("java.util.GregorianCalendar");
+		
+		Class<?> actual = ci.resolve();
+		assertEquals(actual, GregorianCalendar.class);
+	}
+	
+	@Test
+	public void resolveReturnsNull_whenTypeDoesntExist() {
+		ci = new ConditionalInstantiator("this.type.does.not.Exist");
+		
+		Class<?> actual = ci.resolve();
+		assertNull(actual);
+	}
+	
+	@Test
+	public void objectIsInstantiatedCorrectly_whenValidConstructorParametersAreProvided() {
+		ci = new ConditionalInstantiator("java.util.GregorianCalendar");
+		Object expected = new GregorianCalendar(1999, 11, 31);
+
+		Object actual = ci.instantiate(classes(int.class, int.class, int.class), objects(1999, 11, 31));
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void throwsISE_whenInvalidConstructorParametersAreProvided() {
+		ci = new ConditionalInstantiator("java.util.GregorianCalendar");		
+
+		thrown.expect(ReflectionException.class);
+		ci.instantiate(classes(int.class, int.class, int.class), objects(1999, 31, "hello"));
+	}
+	
+	@Test
+	public void objectIsInstantiatedCorrectly_whenValidFactoryMethodAndParametersAreProvided() {
+		ci = new ConditionalInstantiator("java.lang.Integer");
+		Object expected = Integer.valueOf(42);
+		
+		Object actual = ci.callFactory("valueOf", classes(int.class), objects(42));
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void throwsISE_whenInvalidMethodNameIsProvided() {
+		ci = new ConditionalInstantiator("java.lang.Integer");
+		
+		thrown.expect(ReflectionException.class);
+		ci.callFactory("thisMethodDoesntExist", classes(int.class), objects(42));
+	}
+	
+	@Test
+	public void throwsISE_whenInvalidFactoryMethodParametersAreProvided() {
+		ci = new ConditionalInstantiator("java.lang.Integer");
+		
+		thrown.expect(ReflectionException.class);
+		ci.callFactory("valueOf", classes(int.class, int.class), objects(42));
+	}
+	
+	@Test
+	public void objectIsReturned_whenValidConstantIsProvided() {
+		ci = new ConditionalInstantiator("java.math.BigDecimal");
+		Object expected = BigDecimal.TEN;
+		
+		Object actual = ci.returnConstant("TEN");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void throwsISE_whenConstantDoesNotExist() {
+		ci = new ConditionalInstantiator("java.math.BigDecimal");
+		
+		thrown.expect(ReflectionException.class);
+		ci.returnConstant("FORTY-TWO");
+	}
+}
