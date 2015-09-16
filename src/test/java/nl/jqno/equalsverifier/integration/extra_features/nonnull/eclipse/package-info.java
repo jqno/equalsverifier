be7ab2cb574abe92bccb879e59d9361a0a330873
Jan Ouwens <jan.ouwens@gmail.com>
2015-09-16 08:12:13 +0200

    Improve Javadoc for Eclipse nonnull annotation package

--- src/test/java/nl/jqno/equalsverifier/integration/extra_features/nonnull/eclipse/package-info.java
+++ src/test/java/nl/jqno/equalsverifier/integration/extra_features/nonnull/eclipse/package-info.java
@@ -15,6 +15,9 @@
  */
 /**
  * Applies Eclipse's NonNullByDefault annotation to the package.
+ *
+ * Actual classes in this package reside in AnnotationNonnullTypeUseTest,
+ * because they're Java 8 only and need to be compiled conditionally.
  */
 @NonNullByDefault
 package nl.jqno.equalsverifier.integration.extra_features.nonnull.eclipse;
