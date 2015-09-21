59a51405bbbffa6ddd7f469192e2f096cf2ff322
Jan Ouwens <jan.ouwens@gmail.com>
2015-09-21 17:45:24 +0200

    Issue 84: resolve TypeTag from Field

--- src/main/java/nl/jqno/equalsverifier/internal/TypeTag.java
+++ src/main/java/nl/jqno/equalsverifier/internal/TypeTag.java
@@ -15,15 +15,46 @@
  */
 package nl.jqno.equalsverifier.internal;
 
+import nl.jqno.equalsverifier.internal.exceptions.EqualsVerifierBugException;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class TypeTag {
     private final Class<?> type;
     private final List<TypeTag> genericTypes;
 
-    public TypeTag(Class<?> type, List<TypeTag> genericTypes) {
+    public TypeTag(Class<?> type, TypeTag... genericTypes) {
+        this(type, Arrays.asList(genericTypes));
+    }
+
+    private TypeTag(Class<?> type, List<TypeTag> genericTypes) {
         this.type = type;
         this.genericTypes = genericTypes;
+    }
+
+    public static TypeTag of(Field field) {
+        return resolve(field.getGenericType());
+    }
+
+    private static TypeTag resolve(Type type) {
+        List<TypeTag> nestedTags = new ArrayList<>();
+        if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType)type;
+            Type[] typeArgs = pt.getActualTypeArguments();
+            for (Type typeArg : typeArgs) {
+                nestedTags.add(resolve(typeArg));
+            }
+            return new TypeTag((Class)pt.getRawType(), nestedTags);
+        }
+        else if (type instanceof Class) {
+            return new TypeTag((Class)type, nestedTags);
+        }
+        throw new EqualsVerifierBugException("Failed to tag type " + type.toString());
     }
 
     @Override
@@ -41,5 +72,23 @@ public final class TypeTag {
         result = (59 * result) + type.hashCode();
         result = (59 * result) + genericTypes.hashCode();
         return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer s = new StringBuffer("TypeTag: ");
+        s.append(type.getSimpleName());
+        if (genericTypes.size() >= 1) {
+            s.append("<");
+            s.append(genericTypes.get(0));
+        }
+        for (int i = 1; i < genericTypes.size(); i++) {
+            s.append(", ");
+            s.append(genericTypes.get(i));
+        }
+        if (genericTypes.size() >= 1) {
+            s.append(">");
+        }
+        return s.toString();
     }
 }
