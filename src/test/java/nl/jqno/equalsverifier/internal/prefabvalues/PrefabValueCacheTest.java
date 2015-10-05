5082295b4829b89180a9374b552b3ec461a07cf2
Jan Ouwens <jan.ouwens@gmail.com>
2015-10-05 17:02:24 +0200

    Issue 84: Add PrefabValueCache

---
+++ src/test/java/nl/jqno/equalsverifier/internal/prefabvalues/PrefabValueCacheTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 Jan Ouwens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.jqno.equalsverifier.internal.prefabvalues;
+
+import nl.jqno.equalsverifier.internal.TypeTag;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PrefabValueCacheTest {
+    private static final TypeTag STRING_TAG = new TypeTag(String.class);
+    private static final PrefabValueTuple<String> STRING_TUPLE = new PrefabValueTuple<>("red", "black");
+    private static final TypeTag INT_TAG = new TypeTag(int.class);
+    private static final PrefabValueTuple<Integer> INT_TUPLE = new PrefabValueTuple<>(42, 1337);
+
+    private PrefabValueCache cache = new PrefabValueCache();
+
+    @Test
+    public void putAndGetTuple() {
+        cache.put(STRING_TAG, STRING_TUPLE.getRed(), STRING_TUPLE.getBlack());
+        assertEquals(STRING_TUPLE, cache.getTuple(STRING_TAG));
+    }
+
+    @Test
+    public void putTwiceAndGetBoth() {
+        cache.put(STRING_TAG, STRING_TUPLE.getRed(), STRING_TUPLE.getBlack());
+        cache.put(INT_TAG, INT_TUPLE.getRed(), INT_TUPLE.getBlack());
+
+        assertEquals(INT_TUPLE, cache.getTuple(INT_TAG));
+        assertEquals(STRING_TUPLE, cache.getTuple(STRING_TAG));
+    }
+
+    @Test
+    public void contains() {
+        cache.put(STRING_TAG, STRING_TUPLE.getRed(), STRING_TUPLE.getBlack());
+        assertTrue(cache.contains(STRING_TAG));
+    }
+
+    @Test
+    public void doesntContain() {
+        assertFalse(cache.contains(STRING_TAG));
+    }
+}
