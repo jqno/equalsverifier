a253bb01aa0875461395d3202354a8ee9b06e805
NRBPerdijk <nathanperdijk@zoho.com>
2017-08-15 15:19:09 +0200

    Changed copyright on the new files to the name of the file-creator (myself).

--- src/test/java/nl/jqno/equalsverifier/internal/util/FieldNameExtractorTest.java
+++ src/test/java/nl/jqno/equalsverifier/internal/util/FieldNameExtractorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Jan Ouwens
+ * Copyright 2017 Nathan Ragnar Bernard Perdijk
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
