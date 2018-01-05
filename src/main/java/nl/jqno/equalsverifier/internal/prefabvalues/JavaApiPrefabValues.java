519708987c84186a95fdc73978c0da5795f1541d
Jan Ouwens <jan.ouwens@gmail.com>
2018-01-05 07:27:09 +0100

    Does the yearly copyright forgetting dance

--- src/main/java/nl/jqno/equalsverifier/internal/prefabvalues/JavaApiPrefabValues.java
+++ src/main/java/nl/jqno/equalsverifier/internal/prefabvalues/JavaApiPrefabValues.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2017 Jan Ouwens
+ * Copyright 2010-2018 Jan Ouwens
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
