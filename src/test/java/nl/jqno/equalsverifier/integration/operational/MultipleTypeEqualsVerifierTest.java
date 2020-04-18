3be816d7f3c6ee5714c2f958ff0c695ac5ace66c
Jan Ouwens <jan.ouwens@gmail.com>
2020-04-18 09:27:43 +0200

    Multiple types: adds test cases for #report()

--- src/test/java/nl/jqno/equalsverifier/integration/operational/MultipleTypeEqualsVerifierTest.java
+++ src/test/java/nl/jqno/equalsverifier/integration/operational/MultipleTypeEqualsVerifierTest.java
@@ -1,6 +1,14 @@
 package nl.jqno.equalsverifier.integration.operational;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.EqualsVerifierReport;
 import nl.jqno.equalsverifier.testhelpers.ExpectedExceptionTestBase;
 import nl.jqno.equalsverifier.testhelpers.packages.correct.*;
 import nl.jqno.equalsverifier.testhelpers.packages.twoincorrect.*;
@@ -37,5 +45,52 @@ public class MultipleTypeEqualsVerifierTest extends ExpectedExceptionTestBase {
                 "Reflexivity: object does not equal itself:");
 
         EqualsVerifier.forClasses(A.class, IncorrectM.class, C.class, IncorrectN.class).verify();
+    }
+
+    @Test
+    public void succeed_whenReportingOnSeveralCorrectClasses() {
+        List<EqualsVerifierReport> reports =
+                EqualsVerifier.forClasses(A.class, B.class, C.class).report();
+
+        assertEquals(3, reports.size());
+        assertSuccessful(reports.get(0), A.class);
+        assertSuccessful(reports.get(1), B.class);
+        assertSuccessful(reports.get(2), C.class);
+    }
+
+    @Test
+    public void fail_whenReportingOnOneIncorrectClass() {
+        List<EqualsVerifierReport> reports =
+                EqualsVerifier.forClasses(A.class, IncorrectM.class, C.class).report();
+
+        assertEquals(3, reports.size());
+        assertSuccessful(reports.get(0), A.class);
+        assertSuccessful(reports.get(2), C.class);
+        assertUnsuccessful(reports.get(1), IncorrectM.class, "Subclass: equals is not final.");
+    }
+
+    @Test
+    public void fail_whenReportingOnTwoIncorrectClasses() {
+        List<EqualsVerifierReport> reports =
+                EqualsVerifier.forClasses(A.class, IncorrectM.class, C.class, IncorrectN.class)
+                        .report();
+
+        assertEquals(4, reports.size());
+        assertSuccessful(reports.get(0), A.class);
+        assertSuccessful(reports.get(2), C.class);
+        assertUnsuccessful(reports.get(1), IncorrectM.class, "Subclass: equals is not final.");
+        assertUnsuccessful(
+                reports.get(3), IncorrectN.class, "Reflexivity: object does not equal itself:");
+    }
+
+    private void assertSuccessful(EqualsVerifierReport report, Class<?> type) {
+        assertTrue(report.isSuccessful());
+        assertEquals(type, report.getType());
+    }
+
+    private void assertUnsuccessful(EqualsVerifierReport report, Class<?> type, String message) {
+        assertFalse(report.isSuccessful());
+        assertEquals(type, report.getType());
+        assertThat(report.getMessage(), containsString(message));
     }
 }
