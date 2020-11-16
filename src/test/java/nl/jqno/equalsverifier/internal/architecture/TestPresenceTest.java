896ad43e83a21cad4484a0a90042f18d7f61fb17
Jan Ouwens <jan.ouwens@gmail.com>
2020-11-16 08:47:18 +0100

    Multi-release: adds test that verifies that the desired tests are indeed present on the desired JVMs

---
+++ src/test/java/nl/jqno/equalsverifier/internal/architecture/TestPresenceTest.java
@@ -0,0 +1,39 @@
+package nl.jqno.equalsverifier.internal.architecture;
+
+import static org.junit.Assert.assertTrue;
+
+import nl.jqno.equalsverifier.internal.reflection.Util;
+import org.junit.Test;
+
+public final class TestPresenceTest {
+    @Test
+    public void javafxTestsArePresentOnJdk11() {
+        assertTrue(
+                isTestAvailableOnJdk(
+                        11,
+                        "nl.jqno.equalsverifier.integration.extended_contract.JavaFxClassesTest"));
+    }
+
+    @Test
+    public void recordTestsArePresentOnJdk16() {
+        assertTrue(
+                isTestAvailableOnJdk(
+                        16, "nl.jqno.equalsverifier.integration.extended_contract.RecordsTest"));
+    }
+
+    private boolean isTestAvailableOnJdk(int jdkVersion, String className) {
+        boolean isJdk = isAtLeastJdk(jdkVersion);
+        boolean available = isClassAvailable(className);
+        return isJdk == available;
+    }
+
+    private boolean isClassAvailable(String className) {
+        return Util.classForName(className) != null;
+    }
+
+    private boolean isAtLeastJdk(int requiredVersion) {
+        String jdkVersion = System.getProperty("java.version");
+        int actualVersion = Integer.parseInt(jdkVersion.split("\\.|-")[0]);
+        return requiredVersion <= actualVersion;
+    }
+}
