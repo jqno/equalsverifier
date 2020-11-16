94520f04a1edea65f8acca6a1fee91f8099bbc2d
Jan Ouwens <jan.ouwens@gmail.com>
2020-11-16 16:17:55 +0100

    JUnit5: fixes parameterized tests

--- src/test/java/nl/jqno/equalsverifier/coverage/CoverageWithInheritanceTest.java
+++ src/test/java/nl/jqno/equalsverifier/coverage/CoverageWithInheritanceTest.java
@@ -1,66 +1,82 @@
 package nl.jqno.equalsverifier.coverage;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Stream;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.testhelpers.types.Color;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Parameterized.class)
-public class CoverageWithInheritanceTest<T, U extends T, V extends U> {
-    private final Class<?> containerType;
-    private final Class<T> superType;
-    private final Class<U> subType;
-    private final Class<V> endpointType;
+public class CoverageWithInheritanceTest {
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public CoverageWithInheritanceTest(Class<?> containerType) {
-        this.containerType = containerType;
-        Class[] containingTypes = containerType.getClasses();
-        this.superType = find(containingTypes, "Point");
-        this.subType = find(containingTypes, "ColorPoint");
-        this.endpointType = find(containingTypes, "EndPoint");
+    public static Stream<Arguments> data() {
+        return Stream.of(Classes.of(HandwrittenCanEqual.class), Classes.of(LombokCanEqual.class));
     }
 
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {{HandwrittenCanEqual.class}, {LombokCanEqual.class}});
+    @ParameterizedTest
+    @MethodSource("data")
+    public <T> void testSuperCoverage(Classes<T> classes) {
+        EqualsVerifier.forClass(classes.superType).withRedefinedSubclass(classes.subType).verify();
     }
 
-    @SuppressWarnings("rawtypes")
-    private Class find(Class[] types, String name) {
-        Class result = null;
-        for (Class type : types) {
-            if (type.getSimpleName().equals(name)) {
-                result = type;
-            }
-        }
-        return result;
-    }
-
-    @Test
-    public void testSuperCoverage() {
-        EqualsVerifier.forClass(superType).withRedefinedSubclass(subType).verify();
-    }
-
-    @Test
-    public void testSubCoverage() {
-        EqualsVerifier.forClass(subType)
+    @ParameterizedTest
+    @MethodSource("data")
+    public <T> void testSubCoverage(Classes<T> classes) {
+        EqualsVerifier.forClass(classes.subType)
                 .withRedefinedSuperclass()
-                .withRedefinedSubclass(endpointType)
+                .withRedefinedSubclass(classes.endpointType)
                 .verify();
     }
 
-    @Test
-    public void callTheConstructors() throws Exception {
-        containerType.getConstructor().newInstance();
-        superType.getConstructor(int.class, int.class).newInstance(0, 0);
-        subType.getConstructor(int.class, int.class, Color.class).newInstance(0, 0, Color.INDIGO);
-        endpointType
+    @ParameterizedTest
+    @MethodSource("data")
+    public void callTheConstructors(Classes<?> classes) throws Exception {
+        classes.containerType.getConstructor().newInstance();
+        classes.superType.getConstructor(int.class, int.class).newInstance(0, 0);
+        classes.subType
                 .getConstructor(int.class, int.class, Color.class)
                 .newInstance(0, 0, Color.INDIGO);
+        classes.endpointType
+                .getConstructor(int.class, int.class, Color.class)
+                .newInstance(0, 0, Color.INDIGO);
+    }
+
+    private static final class Classes<T> {
+        private final Class<?> containerType;
+        private final Class<? super T> superType;
+        private final Class<T> subType;
+        private final Class<? extends T> endpointType;
+
+        private Classes(
+                Class<?> containerType,
+                Class<? super T> superType,
+                Class<T> subType,
+                Class<? extends T> endpointType) {
+            this.containerType = containerType;
+            this.superType = superType;
+            this.subType = subType;
+            this.endpointType = endpointType;
+        }
+
+        @SuppressWarnings("unchecked")
+        public static <T> Arguments of(Class<?> containerType) {
+            Class<?>[] containingTypes = containerType.getClasses();
+            Class<? super T> superType = find(containingTypes, "Point");
+            Class<T> subType = find(containingTypes, "ColorPoint");
+            Class<? extends T> endpointType = find(containingTypes, "EndPoint");
+            Classes<T> result = new Classes<>(containerType, superType, subType, endpointType);
+            return Arguments.of(result);
+        }
+
+        @SuppressWarnings("rawtypes")
+        private static Class find(Class[] types, String name) {
+            Class result = null;
+            for (Class type : types) {
+                if (type.getSimpleName().equals(name)) {
+                    result = type;
+                }
+            }
+            return result;
+        }
     }
 }
