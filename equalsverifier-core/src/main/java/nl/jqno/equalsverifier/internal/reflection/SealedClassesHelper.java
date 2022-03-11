bf7267051d2bdefd87a9773ce9fb64f0c01716f7
Jan Ouwens <jan.ouwens@gmail.com>
2022-03-11 10:29:41 +0100

    Adds Javadoc to MRJAR-specific classes

--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/reflection/SealedClassesHelper.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/reflection/SealedClassesHelper.java
@@ -1,5 +1,9 @@
 package nl.jqno.equalsverifier.internal.reflection;
 
+/**
+ * Note: this is a generic implementation for a multi-release jar class.
+ * See equalsverifier-17 submodule.
+ */
 public final class SealedClassesHelper {
 
     private SealedClassesHelper() {}
