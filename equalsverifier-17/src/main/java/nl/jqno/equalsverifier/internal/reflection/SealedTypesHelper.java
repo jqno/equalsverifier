95cf2e85294473f911689af7523270289a085991
Jan Ouwens <jan.ouwens@gmail.com>
2023-02-22 10:18:52 +0100

    Sealed types: adds logic to find implementable subclass

--- equalsverifier-17/src/main/java/nl/jqno/equalsverifier/internal/reflection/SealedTypesHelper.java
+++ equalsverifier-17/src/main/java/nl/jqno/equalsverifier/internal/reflection/SealedTypesHelper.java
@@ -1,10 +1,44 @@
 package nl.jqno.equalsverifier.internal.reflection;
 
+import java.lang.reflect.Modifier;
+import java.util.Optional;
+import nl.jqno.equalsverifier.internal.exceptions.EqualsVerifierInternalBugException;
+
 public final class SealedTypesHelper {
 
     private SealedTypesHelper() {}
 
     public static boolean isSealed(Class<?> type) {
         return type.isSealed();
+    }
+
+    public static Optional<Class<?>> findInstantiableSubclass(Class<?> type) {
+        return findInstantiablePermittedClass(type, false);
+    }
+
+    private static Optional<Class<?>> findInstantiablePermittedClass(
+        Class<?> type,
+        boolean checkCurrent
+    ) {
+        if (checkCurrent && (!isAbstract(type) || !type.isSealed())) {
+            return Optional.of(type);
+        }
+        var permittedSubclasses = type.getPermittedSubclasses();
+        if (permittedSubclasses == null) {
+            return Optional.empty();
+        }
+        for (Class<?> subType : permittedSubclasses) {
+            var c = findInstantiablePermittedClass(subType, true);
+            if (c.isPresent()) {
+                return c;
+            }
+        }
+        throw new EqualsVerifierInternalBugException(
+            "Could not find a non-sealed subtype for " + type.getCanonicalName()
+        );
+    }
+
+    private static boolean isAbstract(Class<?> type) {
+        return Modifier.isAbstract(type.getModifiers());
     }
 }
--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/reflection/SealedTypesHelper.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/reflection/SealedTypesHelper.java
@@ -1,5 +1,7 @@
 package nl.jqno.equalsverifier.internal.reflection;
 
+import java.util.Optional;
+
 /**
  * Note: this is a generic implementation for a multi-release jar class.
  * See equalsverifier-17 submodule.
@@ -10,5 +12,9 @@ public final class SealedTypesHelper {
 
     public static boolean isSealed(Class<?> type) {
         return false;
+    }
+
+    public static Optional<Class<?>> findConcreteImplementation(Class<?> type) {
+        return Optional.empty();
     }
 }
