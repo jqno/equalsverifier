bc2b06e019bb36c625860782f65e2a366e0eb17d
Jan Ouwens <jan.ouwens@gmail.com>
2023-11-16 08:08:47 +0100

    Adds equalsverifier-21 module for testing

---
+++ equalsverifier-21/src/test/java/nl/jqno/equalsverifier/integration/extended_contract/RecordPatternMatchTest.java
@@ -0,0 +1,28 @@
+package nl.jqno.equalsverifier.integration.extended_contract;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.internal.testhelpers.Util;
+import org.junit.jupiter.api.Test;
+
+public class RecordPatternMatchTest {
+
+    @Test
+    void succeed_whenEqualsUsesInstanceofPatternMatch() {
+        EqualsVerifier.forClass(Point.class).verify();
+    }
+
+    record Point(int x, int y) {
+        @Override
+        public boolean equals(Object obj) {
+            return (
+                this == obj ||
+                (obj instanceof Point(int otherX, int otherY) && x == otherX && y == otherY)
+            );
+        }
+
+        @Override
+        public int hashCode() {
+            return Util.defaultHashCode(this);
+        }
+    }
+}
