e2221546ccf2cf98fac034010c5acccb80d1814a
Jan Ouwens <jan.ouwens@gmail.com>
2024-03-08 20:07:31 +0100

    Introduces SubjectCreator in several FieldChecks

--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/checkers/fieldchecks/TransitivityFieldCheck.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/checkers/fieldchecks/TransitivityFieldCheck.java
@@ -4,37 +4,26 @@ import static nl.jqno.equalsverifier.internal.util.Assert.fail;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.lang.reflect.Field;
-import nl.jqno.equalsverifier.internal.prefabvalues.PrefabValues;
-import nl.jqno.equalsverifier.internal.prefabvalues.TypeTag;
-import nl.jqno.equalsverifier.internal.reflection.FieldAccessor;
-import nl.jqno.equalsverifier.internal.reflection.FieldIterable;
-import nl.jqno.equalsverifier.internal.reflection.ObjectAccessor;
+import nl.jqno.equalsverifier.internal.instantiation.SubjectCreator;
 import nl.jqno.equalsverifier.internal.util.Formatter;
 
 public class TransitivityFieldCheck<T> implements FieldCheck<T> {
 
-    private final PrefabValues prefabValues;
-    private final TypeTag typeTag;
+    private final SubjectCreator<T> subjectCreator;
 
     @SuppressFBWarnings(
         value = "EI_EXPOSE_REP2",
         justification = "PrefabValues is inherently mutable."
     )
-    public TransitivityFieldCheck(PrefabValues prefabValues, TypeTag typeTag) {
-        this.prefabValues = prefabValues;
-        this.typeTag = typeTag;
+    public TransitivityFieldCheck(SubjectCreator<T> subjectCreator) {
+        this.subjectCreator = subjectCreator;
     }
 
     @Override
-    public void execute(
-        ObjectAccessor<T> referenceAccessor,
-        ObjectAccessor<T> copyAccessor,
-        FieldAccessor fieldAccessor
-    ) {
-        Field field = fieldAccessor.getField();
-        T a1 = referenceAccessor.get();
-        T b1 = buildB1(copyAccessor, field);
-        T b2 = buildB2(referenceAccessor, field);
+    public void execute(Field changedField) {
+        T a1 = subjectCreator.plain();
+        T b1 = subjectCreator.withFieldChanged(changedField);
+        T b2 = subjectCreator.withAllFieldsChanged();
 
         boolean x = a1.equals(b1);
         boolean y = b1.equals(b2);
@@ -51,23 +40,6 @@ public class TransitivityFieldCheck<T> implements FieldCheck<T> {
                 )
             );
         }
-    }
-
-    private T buildB1(ObjectAccessor<T> accessor, Field field) {
-        accessor.withChangedField(field, prefabValues, typeTag);
-        return accessor.get();
-    }
-
-    private T buildB2(ObjectAccessor<T> referenceAccessor, Field field) {
-        T copy = referenceAccessor.copy();
-        ObjectAccessor<T> objectAccessor = ObjectAccessor.of(copy);
-        objectAccessor = objectAccessor.withChangedField(field, prefabValues, typeTag);
-        for (Field f : FieldIterable.of(referenceAccessor.type())) {
-            if (!f.equals(field)) {
-                objectAccessor = objectAccessor.withChangedField(f, prefabValues, typeTag);
-            }
-        }
-        return objectAccessor.get();
     }
 
     private int countFalses(boolean... bools) {
