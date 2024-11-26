bd52b93f4a6d2e1ad9f0b180cfe3248453dae2cb
Jan Ouwens <jan.ouwens@gmail.com>
2024-11-26 21:49:38 +0100

    Fixes name

--- equalsverifier-17/src/test/java/nl/jqno/equalsverifier/internal/reflection/ClassProbeSealedTest.java
+++ equalsverifier-17/src/test/java/nl/jqno/equalsverifier/internal/reflection/ClassProbeSealedTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class ClassAccessorSealedTest {
+public class ClassProbeSealedTest {
 
     @Test
     public void isNotSealed() {
