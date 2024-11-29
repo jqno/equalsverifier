486c14464f1e64d6dcd5270e1d076b7547c49588
Jan Ouwens <jan.ouwens@gmail.com>
2024-11-29 15:19:40 +0100

    Removes unused helper code

--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/testhelpers/TestValueProviders.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/testhelpers/TestValueProviders.java
@@ -1,19 +1,11 @@
 package nl.jqno.equalsverifier.internal.testhelpers;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Optional;
 import nl.jqno.equalsverifier.internal.reflection.Tuple;
 import nl.jqno.equalsverifier.internal.reflection.TypeTag;
 import nl.jqno.equalsverifier.internal.reflection.instantiation.ValueProvider;
 
-@SuppressFBWarnings(
-    value = "DM_STRING_CTOR",
-    justification = "We really do need a separate instances with the same value"
-)
 public final class TestValueProviders {
-
-    public static final Tuple<Integer> INTS = Tuple.of(42, 1337, 42);
-    public static final Tuple<String> STRINGS = Tuple.of("abc", "xyz", new String("abc"));
 
     private TestValueProviders() {}
 
@@ -21,25 +13,6 @@ public final class TestValueProviders {
         return new ValueProvider() {
             @Override
             public <T> Optional<Tuple<T>> provide(TypeTag tag, Attributes attributes) {
-                return Optional.empty();
-            }
-        };
-    }
-
-    public static ValueProvider simple() {
-        return new ValueProvider() {
-            @SuppressWarnings("unchecked")
-            @Override
-            public <T> Optional<Tuple<T>> provide(TypeTag tag, Attributes attributes) {
-                if (tag.getType().equals(int.class)) {
-                    return Optional.of((Tuple<T>) INTS);
-                }
-                if (tag.getType().equals(Integer.class)) {
-                    return Optional.of((Tuple<T>) INTS);
-                }
-                if (tag.getType().equals(String.class)) {
-                    return Optional.of((Tuple<T>) STRINGS);
-                }
                 return Optional.empty();
             }
         };
