b8241357151a622468e77b9be9318f90eef65a86
Jan Ouwens <jan.ouwens@gmail.com>
2024-12-27 21:49:41 +0100

    Some more simplifications

--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/instantiation/vintage/prefabvalues/factories/EnumMapFactory.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/instantiation/vintage/prefabvalues/factories/EnumMapFactory.java
@@ -1,6 +1,5 @@
 package nl.jqno.equalsverifier.internal.instantiation.vintage.prefabvalues.factories;
 
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.function.Function;
@@ -9,7 +8,7 @@ import nl.jqno.equalsverifier.internal.instantiation.vintage.VintageValueProvide
 import nl.jqno.equalsverifier.internal.reflection.Tuple;
 import nl.jqno.equalsverifier.internal.reflection.TypeTag;
 
-@SuppressWarnings({ "unchecked", "rawtypes" })
+@SuppressWarnings("rawtypes")
 public final class EnumMapFactory<T> extends AbstractGenericFactory<T> {
 
     private final Function<Map, T> factory;
@@ -24,12 +23,9 @@ public final class EnumMapFactory<T> extends AbstractGenericFactory<T> {
         TypeTag keyTag = determineAndCacheActualTypeTag(0, tag, valueProvider, clone, Enum.class);
         TypeTag valueTag = determineAndCacheActualTypeTag(1, tag, valueProvider, clone, Enum.class);
 
-        Map red = new HashMap<>();
-        Map blue = new HashMap<>();
-        Map redCopy = new HashMap<>();
-        red.put(valueProvider.giveRed(keyTag), valueProvider.giveBlue(valueTag));
-        blue.put(valueProvider.giveBlue(keyTag), valueProvider.giveBlue(valueTag));
-        redCopy.put(valueProvider.giveRed(keyTag), valueProvider.giveBlue(valueTag));
+        Map red = Map.of(valueProvider.giveRed(keyTag), valueProvider.giveBlue(valueTag));
+        Map blue = Map.of(valueProvider.giveBlue(keyTag), valueProvider.giveBlue(valueTag));
+        Map redCopy = Map.of(valueProvider.giveRed(keyTag), valueProvider.giveBlue(valueTag));
 
         return Tuple.of(factory.apply(red), factory.apply(blue), factory.apply(redCopy));
     }
