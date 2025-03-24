7d5367cf620644b3ecc4b1dd3693a635a72be0ec
Jan Ouwens <jan.ouwens@gmail.com>
2025-03-24 21:16:25 +0100

    Adds operational tests for Mockito

---
+++ equalsverifier-test-mockito/src/test/java/nl/jqno/equalsverifier/mockito/MockitoDisableTest.java
@@ -0,0 +1,35 @@
+package nl.jqno.equalsverifier.mockito;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Mode;
+import nl.jqno.equalsverifier_testhelpers.ExpectedException;
+import nl.jqno.equalsverifier_testhelpers.types.RecursiveTypeHelper.Node;
+import nl.jqno.equalsverifier_testhelpers.types.RecursiveTypeHelper.NodeContainer;
+import org.junit.jupiter.api.Test;
+
+public class MockitoDisableTest {
+    @Test
+    void singleTypeDisable() {
+        ExpectedException
+                .when(() -> EqualsVerifier.forClass(NodeContainer.class).set(Mode.skipMockito()).verify())
+                .assertFailure()
+                .assertMessageContains("Recursive datastructure");
+    }
+
+    @Test
+    void multipleTypeDisable() {
+        ExpectedException
+                .when(() -> EqualsVerifier.forClasses(NodeContainer.class, Node.class).set(Mode.skipMockito()).verify())
+                .assertFailure()
+                .assertMessageContains("Recursive datastructure");
+    }
+
+    @Test
+    void configuredEqualsVerifierDisable() {
+        var ev = EqualsVerifier.configure().set(Mode.skipMockito());
+        ExpectedException
+                .when(() -> ev.forClass(NodeContainer.class).verify())
+                .assertFailure()
+                .assertMessageContains("Recursive datastructure");
+    }
+}
--- equalsverifier-test-mockito/src/test/java/nl/jqno/equalsverifier/mockito/SanityTest.java
+++ equalsverifier-test-mockito/src/test/java/nl/jqno/equalsverifier/mockito/SanityTest.java
@@ -3,6 +3,9 @@ package nl.jqno.equalsverifier.mockito;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier_testhelpers.ExpectedException;
+import nl.jqno.equalsverifier_testhelpers.types.GetClassPoint;
 import nl.jqno.equalsverifier_testhelpers.types.Point;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +16,13 @@ public class SanityTest {
         var b = mock(Point.class);
 
         assertThat(a).isNotEqualTo(b);
+    }
+
+    @Test
+    void incorrectEqualsIsStillDetected() {
+        ExpectedException
+                .when(() -> EqualsVerifier.forClass(GetClassPoint.class).verify())
+                .assertFailure()
+                .assertMessageContains("Subclass:");
     }
 }
