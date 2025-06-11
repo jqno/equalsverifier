ab6072ea1f85c712baf877f04fbd1be12fd59f56
Jan Ouwens <jan.ouwens@gmail.com>
2025-06-11 19:09:44 +0000

    Improves MockitoException error message

--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/exceptions/MockitoException.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/exceptions/MockitoException.java
@@ -3,13 +3,17 @@ package nl.jqno.equalsverifier.internal.exceptions;
 import nl.jqno.equalsverifier.internal.util.Formatter;
 
 public class MockitoException extends MessagingException {
-    public MockitoException(String fieldName, String typeName) {
+    public MockitoException(String fieldName, String typeName, String methodName) {
         super(Formatter
                 .of(
                     """
-                    Unable to use Mockito to mock field %% of type %%.
-                       Please provide a prefab value for this type, or use #set(Mode.skipMockito()) to skip using Mockito altogether.""",
+                    Unable to use Mockito to mock field %%:
+                       tried to call method %%.%%, which EqualsVerifier can't mock.
+                       Please provide a prefab value for type %%,
+                       or use #set(Mode.skipMockito()) to skip using Mockito altogether.""",
                     fieldName,
+                    typeName,
+                    methodName,
                     typeName)
                 .format());
     }
