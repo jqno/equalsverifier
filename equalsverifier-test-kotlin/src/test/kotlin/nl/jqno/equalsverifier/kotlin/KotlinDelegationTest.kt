6b6427763a1a4b4e319966838e802cb7c68168ea
Jan Ouwens <jan.ouwens@gmail.com>
2025-07-10 13:13:57 +0200

    Adds test case for unused Kotlin delegate

--- equalsverifier-test-kotlin/src/test/kotlin/nl/jqno/equalsverifier/kotlin/KotlinDelegationTest.kt
+++ equalsverifier-test-kotlin/src/test/kotlin/nl/jqno/equalsverifier/kotlin/KotlinDelegationTest.kt
@@ -10,6 +10,11 @@ class KotlinDelegationTest {
     EqualsVerifier.forClass(FooContainer::class.java).verify()
   }
 
+  @Test
+  fun `succeed when class ignores interface delegation`() {
+    EqualsVerifier.forClass(FooAndMoreContainer::class.java).verify()
+  }
+
   interface Foo {
     val foo: Int
   }
@@ -25,5 +30,16 @@ class KotlinDelegationTest {
     }
 
     override fun hashCode(): Int = foo
+  }
+
+  class FooAndMoreContainer(fooValue: Int, val bar: Int): Foo by FooImpl(fooValue) {
+
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (other !is FooAndMoreContainer) return false
+      return bar == other.bar
+    }
+
+    override fun hashCode(): Int = bar
   }
 }
