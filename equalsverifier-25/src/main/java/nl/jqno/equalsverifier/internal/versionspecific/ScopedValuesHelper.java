8fb38a16d9de17858103ab6e2d8f588d1827c3b2
Jan Ouwens <jan.ouwens@gmail.com>
2025-09-03 13:21:43 +0200

    Adds prefab values for ScopedValue

--- equalsverifier-25/src/main/java/nl/jqno/equalsverifier/internal/versionspecific/ScopedValuesHelper.java
+++ equalsverifier-25/src/main/java/nl/jqno/equalsverifier/internal/versionspecific/ScopedValuesHelper.java
@@ -1,10 +1,15 @@
 package nl.jqno.equalsverifier.internal.versionspecific;
 
+import static nl.jqno.equalsverifier.internal.instantiation.vintage.factories.Factories.values;
+
 import nl.jqno.equalsverifier.internal.instantiation.vintage.FactoryCache;
 
 public final class ScopedValuesHelper {
 
     private ScopedValuesHelper() {}
 
-    public static void add(FactoryCache factoryCache) {}
+    public static void add(FactoryCache factoryCache) {
+        var red = ScopedValue.newInstance();
+        factoryCache.put(ScopedValue.class, values(red, ScopedValue.newInstance(), red));
+    }
 }
