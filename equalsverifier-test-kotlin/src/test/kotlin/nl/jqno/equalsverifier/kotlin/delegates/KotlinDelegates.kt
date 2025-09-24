87505c080e01e9279049991ac3535db73a5602bc
Jan Ouwens <jan.ouwens@gmail.com>
2025-09-24 20:52:59 +0200

    Separates out Kotlin delegate classes from tests

---
+++ equalsverifier-test-kotlin/src/test/kotlin/nl/jqno/equalsverifier/kotlin/delegates/KotlinDelegates.kt
@@ -0,0 +1,172 @@
+package nl.jqno.equalsverifier.kotlin.delegates
+
+import kotlin.reflect.KProperty
+
+/*
+ * No delegation
+ */
+
+class Normal(val foo: Int) {
+
+  override fun equals(other: Any?): Boolean =
+    (other is Normal) && foo == other.foo
+
+  override fun hashCode(): Int = foo
+}
+
+/*
+ * Interface delegation
+ */
+
+interface Foo {
+  val foo: Int
+}
+
+data class FooImpl(override val foo: Int): Foo
+
+class InterfaceDelegation(fooValue: Int): Foo by FooImpl(fooValue) {
+
+  override fun equals(other: Any?): Boolean =
+    (other is InterfaceDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo
+}
+
+interface TwoFoos {
+  val foo: Int
+  val bar: String
+}
+
+data class TwoFoosImpl(override val foo: Int, override val bar: String): TwoFoos
+
+class TwoFoosDelegationWithExplicitFields(fooValue: Int, barValue: String): TwoFoos by TwoFoosImpl(fooValue, barValue) {
+  override fun equals(other: Any?): Boolean =
+    (other is TwoFoosDelegationWithExplicitFields) && foo == other.foo && bar == other.bar
+
+  override fun hashCode(): Int = (31 * foo) + bar.hashCode()
+}
+
+class TwoFoosDelegationWithParamToClass(baz: TwoFoosImpl): TwoFoos by baz {
+  override fun equals(other: Any?): Boolean =
+    (other is TwoFoosDelegationWithParamToClass) && foo == other.foo && bar == other.bar
+
+  override fun hashCode(): Int = (31 * foo) + bar.hashCode()
+}
+
+class TwoFoosDelegationWithFieldToClass(val baz: TwoFoosImpl): TwoFoos by baz {
+  override fun equals(other: Any?): Boolean =
+    (other is TwoFoosDelegationWithFieldToClass) && foo == other.foo && bar == other.bar
+
+  override fun hashCode(): Int = (31 * foo) + bar.hashCode()
+}
+
+class TwoFoosDelegationWithParamToInterface(baz: TwoFoos): TwoFoos by baz {
+  override fun equals(other: Any?): Boolean =
+    (other is TwoFoosDelegationWithParamToInterface) && foo == other.foo && bar == other.bar
+
+  override fun hashCode(): Int = (31 * foo) + bar.hashCode()
+}
+
+class TwoFoosDelegationWithFieldToInterface(val baz: TwoFoos): TwoFoos by baz {
+  override fun equals(other: Any?): Boolean =
+    (other is TwoFoosDelegationWithFieldToInterface) && foo == other.foo && bar == other.bar
+
+  override fun hashCode(): Int = (31 * foo) + bar.hashCode()
+}
+
+/*
+ * Lazy delegation
+ */
+
+class LazyDelegation(fooValue: Int) {
+  val foo: Int by lazy { fooValue }
+
+  override fun equals(other: Any?): Boolean =
+    (other is LazyDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo
+}
+
+class LazyGenericDelegation(fooValue: List<Int>) {
+  val foo: List<Int> by lazy { fooValue }
+
+  override fun equals(other: Any?): Boolean =
+    (other is LazyGenericDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo.hashCode()
+}
+
+class NestedLazyGenericDelegation(fooValue: List<List<List<Int>>>) {
+  val foo: List<List<List<Int>>> by lazy { fooValue }
+
+  override fun equals(other: Any?): Boolean =
+    (other is NestedLazyGenericDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo.hashCode()
+}
+
+class TwoLazyDelegations(fooValue: Int, barValue: String) {
+  val foo: Int by lazy { fooValue }
+  val bar: String by lazy { barValue }
+
+  override fun equals(other: Any?): Boolean =
+    (other is TwoLazyDelegations) && foo == other.foo && bar == other.bar
+
+  override fun hashCode(): Int = (31 * foo) + bar.hashCode()
+}
+
+/*
+ * Other kinds of delegation
+ */
+
+val topLevelFoo: Int = 42
+
+class TopLevelDelegation {
+  val foo: Int by ::topLevelFoo
+
+  override fun equals(other: Any?): Boolean =
+    (other is TopLevelDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo
+}
+
+class MemberDelegation(val fooValue: Int) {
+  val foo: Int by this::fooValue
+
+  override fun equals(other: Any?): Boolean =
+    (other is MemberDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo
+}
+
+data class AnotherClass(val bar: Int)
+class AnotherClassDelegation(anotherClass: AnotherClass) {
+  val foo: Int by anotherClass::bar
+
+  override fun equals(other: Any?): Boolean =
+    (other is AnotherClassDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo
+}
+
+class MapDelegation(map: Map<String, Any>) {
+  val foo: String by map
+
+  override fun equals(other: Any?): Boolean =
+    (other is MapDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo.hashCode()
+}
+
+class ReflectionDelegationProvider(val seed: Int) {
+  operator fun getValue(thisRef: Any?, prop: KProperty<*>): String =
+    "${prop.name}_${seed}"
+}
+class ReflectionDelegation(seed: Int) {
+  val foo: String by ReflectionDelegationProvider(seed)
+
+  override fun equals(other: Any?): Boolean =
+    (other is ReflectionDelegation) && foo == other.foo
+
+  override fun hashCode(): Int = foo.hashCode()
+}
