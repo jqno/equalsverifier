36861c424f4f3278c0a5a8c1c842a6f66062f2b7
Jan Ouwens <jan.ouwens@gmail.com>
2025-12-19 10:36:25 +0100

    Introduces AbstractValueProvider

---
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/instantiation/AbstractValueProvider.java
@@ -0,0 +1,36 @@
+package nl.jqno.equalsverifier.internal.instantiation;
+
+import java.util.Optional;
+
+import nl.jqno.equalsverifier.internal.reflection.*;
+
+/**
+ * A ValueProvider that can instantiate abstract types, including (sealed) interfaces.
+ */
+public class AbstractValueProvider implements ValueProvider {
+
+    private final ValueProvider vp;
+
+    public AbstractValueProvider(ValueProvider vp) {
+        this.vp = vp;
+    }
+
+    @Override
+    public <T> Optional<Tuple<T>> provide(TypeTag tag, Attributes attributes) {
+        ClassProbe<T> probe = ClassProbe.of(tag.getType());
+        if (!probe.isAbstract()) {
+            return Optional.empty();
+        }
+
+        if (probe.isSealed()) {
+            Class<T> concrete = SealedTypesFinder.findInstantiableSubclass(probe).get();
+            var concreteTag = new TypeTag(concrete);
+            return vp.provide(concreteTag, attributes);
+        }
+        else {
+            Class<T> concrete = Instantiator.giveDynamicSubclass(tag.getType());
+            var concreteTag = new TypeTag(concrete);
+            return vp.provide(concreteTag, attributes);
+        }
+    }
+}
