709b25ca9e7c8805ac539d436a398fc4706f4489
Jan Ouwens <jan.ouwens@gmail.com>
2026-01-23 13:10:33 +0100

    Introduces ProvidedFactoryInstantiator

--- equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/instantiators/InstantiatorFactory.java
+++ equalsverifier-core/src/main/java/nl/jqno/equalsverifier/internal/instantiators/InstantiatorFactory.java
@@ -1,5 +1,6 @@
 package nl.jqno.equalsverifier.internal.instantiators;
 
+import nl.jqno.equalsverifier.InstanceFactory;
 import nl.jqno.equalsverifier.internal.reflection.ClassProbe;
 import org.objenesis.Objenesis;
 
@@ -15,15 +16,36 @@ public final class InstantiatorFactory {
      * Note that the given class is the class that will be instantiated. No subclass will be substituted if the given
      * class is abstract or an interface.
      *
-     * @param <S>       Represents the type of the class to instantiate.
+     * @param <T>       Represents the type of the class to instantiate.
      * @param probe     Represents the class to instantiate.
      * @param objenesis To instantiate non-record classes.
      * @return an {@code InstanceCreator} for the given class.
      */
-    public static <S> Instantiator<S> of(ClassProbe<S> probe, Objenesis objenesis) {
+    public static <T> Instantiator<T> of(ClassProbe<T> probe, Objenesis objenesis) {
+        return of(probe, null, objenesis);
+    }
+
+    /**
+     * Factory method for obtaining an appropriate Instantiator for the class given via the ClassProbe parameter.
+     *
+     * Note that the given class is the class that will be instantiated. No subclass will be substituted if the given
+     * class is abstract or an interface.
+     *
+     * @param <T>       Represents the type of the class to instantiate.
+     * @param probe     Represents the class to instantiate.
+     * @param factory   A factory that can create instances of {@code T}, or null.
+     * @param objenesis To instantiate non-record classes.
+     * @return an {@code InstanceCreator} for the given class.
+     */
+    public static <T> Instantiator<T> of(ClassProbe<T> probe, InstanceFactory<T> factory, Objenesis objenesis) {
+        if (factory != null) {
+            return new ProvidedFactoryInstantiator<>(factory);
+        }
+
         if (probe.isRecord()) {
             return new ConstructorInstantiator<>(probe.getType());
         }
+
         return new ReflectionInstantiator<>(probe, objenesis);
     }
 }
