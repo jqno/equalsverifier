34c2c839e403bb010764e8751b372f186d79f979
Jan Ouwens <jan.ouwens@gmail.com>
2026-01-25 20:10:41 +0100

    Adds WithFactoryTests and fixes bugs

--- equalsverifier-26/src/test/java/nl/jqno/equalsverifier/strictfinal/FinalMeansFinalSanityTest.java
+++ equalsverifier-26/src/test/java/nl/jqno/equalsverifier/strictfinal/FinalMeansFinalSanityTest.java
@@ -3,7 +3,7 @@ package nl.jqno.equalsverifier.strictfinal;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.lang.reflect.Field;
-import java.util.Objects;
+import java.util.List;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
 public class FinalMeansFinalSanityTest {
     @Test
     void sanity() throws Exception {
-        var q = new Foo("foo");
+        var q = new Uninstantiable(List.of("foo"), "bar");
 
-        Field f = Foo.class.getDeclaredField("s");
+        Field f = Uninstantiable.class.getDeclaredField("s");
         f.setAccessible(true);
 
         assertThatThrownBy(() -> f.set(q, "bar")).isInstanceOf(IllegalAccessException.class);
@@ -23,26 +23,8 @@ public class FinalMeansFinalSanityTest {
     void equalsverifier() {
         // Currently, EqualsVerifier still fails.
         // Eventually this test should be changed so the exception is not expected.
-        assertThatThrownBy(() -> EqualsVerifier.forClass(Foo.class).verify())
+        assertThatThrownBy(() -> EqualsVerifier.forClass(Uninstantiable.class).verify())
                 .isInstanceOf(AssertionError.class)
                 .hasMessageContaining("cannot set final field");
-    }
-
-    static final class Foo {
-        final String s;
-
-        public Foo(String s) {
-            this.s = s;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return obj instanceof Foo other && Objects.equals(s, other.s);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(s);
-        }
     }
 }
